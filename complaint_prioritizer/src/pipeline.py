36d099488a857343793f4fe3759c2ed7fcdd8c1e
vatsuvaksi <vatsuvaksi@gmail.com>
2025-04-26 23:46:29 +0530

    002- setting up pipeline

---
+++ complaint_prioritizer/src/pipeline.py
@@ -0,0 +1,9 @@
+# Load data
+# Clean it
+# Tokenize
+# Vectorize
+# Do SOmthing about it now
+# Save processed data
+def run_pipeline():
+    print("Running Pipeline : ")
+    print("Starting Pre Processing of Data")
